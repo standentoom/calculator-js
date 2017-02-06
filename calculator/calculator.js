45f44601406d912aadc87ca6437dbe05052524a8
DESKTOP-EM2NVUS\tooms <99041312@mydavinci.nl>
2017-02-06 10:54:15 +0100

    no message

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,76 +1,114 @@
-var keys = document.querySelectorAll('#calculator span');
-var operators = ['+', '-', 'x', '÷'];
-var decimalAdded = false;
+var Calculator = {};
+var operators = [];
+var numbers = [];
+var input = "";
+var SomDisplay = "";
+var anwser = null;
 
-for(var i = 0; i < keys.length; i++) {
-	keys[i].onclick = function(e) {
-		var input = document.querySelector('.screen');
-		var inputVal = input.innerHTML;
-		var btnVal = this.innerHTML;
+Calculator.clear = function() {
+	operators = [];
+	numbers = [];
+	input = "";
+	SomDisplay = "";
+	anwser = null;
+	Calculator.display();
+}
+
+Calculator.add = function(inputFromCalculator) {
+	SomDisplay += inputFromCalculator; 
+	input = input += inputFromCalculator;
+
+	Calculator.display();
+}
+
+Calculator.operator = function(action) {
+	numbers.push(input);
+	operators.push(action);
+	input = "";
+
+	SomDisplay += " " + action + " ";
+	Calculator.display();
+}
+
+Calculator.calculate = function() {
+	numbers.push(input);
+	numbers.push(anwser);
+
+	if (anwser != null) {
+		var b = numbers[0];
+		numbers[0] = numbers[1];
+		numbers[1] = b;
+	}
+	while ((operators.includes("*") || operators.includes("/"))) {
 		
-		if(btnVal == 'C') {
-			input.innerHTML = '';
-			decimalAdded = false;
-		}
-		
-		else if(btnVal == '=') {
-			var equation = inputVal;
-			var lastChar = equation[equation.length - 1];
-			
-			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
-			
-			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
-				equation = equation.replace(/.$/, '');
-			
-
-
-
-
-
-
-
-			if(equation)
-				input.innerHTML = eval(equation);
-				
-
-
-
-
-
-
-
-
-			decimalAdded = false;
-		}
-		
-		
-		else if(operators.indexOf(btnVal) > -1) {
-			var lastChar = inputVal[inputVal.length - 1];
-			
-			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
-				input.innerHTML += btnVal;
-			
-			else if(inputVal == '' && btnVal == '-') 
-				input.innerHTML += btnVal;
-			
-			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
-				input.innerHTML = inputVal.replace(/.$/, btnVal);
-			}
-			
-			decimalAdded =false;
-		}
-		
-		else if(btnVal == '.') {
-			if(!decimalAdded) {
-				input.innerHTML += btnVal;
-				decimalAdded = true;
+		var keer = operators.indexOf("*");
+		var delen = operators.indexOf("/");
+	
+		var position  = -1;
+	
+		if (((keer < delen) && keer > -1) || (keer >= 0 && delen == -1)) {
+			position = keer;
+		} else {
+			if (delen > -1){
+				position = delen;
 			}
 		}
-		
-		else {
-			input.innerHTML += btnVal;
+	
+		if (operators[position] == "*") {
+			numbers[position] = Number(numbers[position]) * Number(numbers[position + 1]);
+		} else {
+			numbers[position] = Number(numbers[position]) / Number(numbers[position + 1]);
 		}
+	
+		operators.splice(position, 1);
+		numbers.splice(position + 1, 1);
+	}
+	
+	while ((operators.includes("+") || operators.includes("-"))) {
 		
-		e.preventDefault();
-	} 
+		var plus = operators.indexOf("+");
+		var min = operators.indexOf("-");
+	
+		var position  = -1;
+	
+		if (((plus < min) && plus > -1) || (plus >= 0 && min == -1)) {
+			position = plus;
+		} else {
+			if (min > -1){
+				position = min;
+			}
+		}
+	
+		if (operators[position] == "+") {
+			numbers[position] = Number(numbers[position]) + Number(numbers[position + 1]);
+		} else {
+			numbers[position] = Number(numbers[position]) - Number(numbers[position + 1]);
+		}
+	
+		operators.splice(position, 1);
+		numbers.splice(position + 1, 1);
+	}
+
+	anwser = numbers[0];
+	
+	SomDisplay = "(" + SomDisplay + ")";
+
+	Calculator.display(); 
+}
+
+Calculator.display = function() {
+	
+	document.getElementById("DisplaySom").value = SomDisplay;
+
+	
+	var Display = document.getElementById("Display");
+
+	
+	if (anwser != null) {
+		Display.value = Math.round(anwser * 100) / 100;
+		numbers = [];
+	
+	} else {
+		Display.value = 0;
+	}
 }
--- calculator/java.js
+++ calculator/java.js
@@ -1,6 +1,87 @@
-var.calculator = {}
-var.opperators = []
-var numbers = [];
-var input = "";
-var SomDisplay = "";
-var anwser = null;
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("number1");
+button1.addEventListener("click", pressed1);
+
+
+function pressed1 (parms){
+	
+	console.log("pressed");
+}
+var button1 =document.getElementById("number2");
+button1.addEventListener("click", pressed1);
+
+function pressed1 (parms){
+
+	console.log("pressed");
+}
+var button1 =document.getElementById("number3");
+button1.addEventListener("click", pressed1);
+
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("number4");
+button1.addEventListener("click", pressed1);
+
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("number5");
+button1.addEventListener("click", pressed1);
+
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("number6");
+button1.addEventListener("click", pressed1);
+
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("number7");
+button1.addEventListener("click", pressed1);
+
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("number8");
+button1.addEventListener("click", pressed1);
+
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("number9");
+button1.addEventListener("click", pressed1);
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("number0");
+button1.addEventListener("click", pressed1);
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("dot");
+button1.addEventListener("click", pressed1);
+
+
+
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("x");
+button1.addEventListener("click", pressed1);
+function pressed1 (parms){
+button1.addEventListener("click", pressed1);
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("minnus");
+button1.addEventListener("click", pressed1);
+
+function pressed1 (parms){
+	console.log("pressed");
+}
+var button1 =document.getElementById("isses");
+button1.addEventListener("click", pressed1);
